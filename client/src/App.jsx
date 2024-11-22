7d0aeb1390eb4b1c2cc1b57afe9c1d7f755821c8
vinodpatil2002 <vinodnspatil@gmail.com>
2024-11-22 23:40:08 +0530

    Add routing with react-router-dom and create Home, Signin, and Signup pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
 export default function App() {
-    return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/sign-in" element={<Signin />} />
+                <Route path="/sign-up" element={<Signup />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
