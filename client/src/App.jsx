523f010653db27ed01dc7eb2e756261e8fc513b4
vinodpatil2002 <vinodnspatil@gmail.com>
2024-11-23 00:11:38 +0530

    Add Header component and integrate react-icons; update package dependencies

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,9 +2,11 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
+import Header from "./components/Header";
 export default function App() {
     return (
         <BrowserRouter>
+            <Header />
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/sign-in" element={<Signin />} />
