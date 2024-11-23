f092829a4e1a07c9bfbb6dc4a2e5908391d6e2c3
vinodpatil2002 <vinodnspatil@gmail.com>
2024-11-23 17:00:26 +0530

    Complete Signin functionality

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,20 +1,55 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const Signin = () => {
+    const navigate = useNavigate();
+    const [formData, setFormData] = useState({});
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+          setLoading(true);
+          setError(null);
+          const res = await fetch('api/user/signin',{
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formData),
+          });
+          const data = await res.json();
+          if(data.success === false){
+            setError(data.message);
+            setLoading(false);
+            return;
+          }
+          setLoading(false);
+          setError(null);
+          navigate('/');
+        } catch (error) {
+            setError(error.response.data || error.message);
+            setLoading(false);
+        }
+    };
     return (
         <div className="p-3 max-w-lg mx-auto">
             <h1 className="text-3xl font-semibold text-center my-7">Sign In</h1>
-            <form className="flex flex-col gap-3">
+            <form onSubmit={handleSubmit} className="flex flex-col gap-3">
                 <input
                     type="text"
                     className="border p-3 rounded-lg"
                     placeholder="username"
+                    onChange={handleChange}
                 />
                 <input
                     type="password"
                     className="border p-3 rounded-lg"
                     placeholder="password"
+                    onChange={handleChange}
                 />
                 <button className="text-white bg-slate-700 hover:opacity-95 rounded-lg p-3 uppercase">
                     Sign In
