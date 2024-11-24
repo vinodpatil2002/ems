78c61005f298e5bfc0226387aad9efe264013e3d
vinodpatil2002 <vinodnspatil@gmail.com>
2024-11-24 15:21:30 +0530

    Complete Image Uploading functionality in AddEmployee

--- client/src/pages/AddEmployee.jsx
+++ client/src/pages/AddEmployee.jsx
@@ -1,6 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+    getDownloadURL,
+    getStorage,
+    ref,
+    uploadBytesResumable,
+} from "firebase/storage";
+import { app } from "../firebase";
 
 const AddEmployee = () => {
+    const [file, setFile] = useState(null);
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        mobile: "",
+        designation: "",
+        gender: "",
+        course: [],
+        image: "",
+    });
+    // console.log(file);
+    console.log(formData);
+    const [uploading, setUploading] = useState(false);
+    const [imageUploadError, setImageUploadError] = useState(false);
+    const storeImage = async (file) => {
+        return new Promise((resolve, reject) => {
+            const storage = getStorage(app);
+            const fileName = new Date().getTime() + file.name;
+            const storageRef = ref(storage, fileName);
+            const uploadTask = uploadBytesResumable(storageRef, file);
+            uploadTask.on(
+                "state_changed",
+                (snapshot) => {
+                    const progress =
+                        (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                    console.log("Upload is " + progress + "% done");
+                },
+                (error) => {
+                    reject(error);
+                },
+                () => {
+                    getDownloadURL(uploadTask.snapshot.ref).then(
+                        (downloadURL) => {
+                            resolve(downloadURL);
+                        }
+                    );
+                }
+            );
+        });
+    };
+    const handleImageSubmit = () => {
+        const promise = storeImage(file);
+        setUploading(true);
+        setImageUploadError(false);
+        promise
+            .then((url) => {
+                setFormData({
+                    ...formData,
+                    image: url,
+                });
+                setImageUploadError(false);
+                setUploading(false);
+            })
+            .catch((error) => {
+                console.log(error);
+                setImageUploadError("Image Upload Failed 2mb max");
+                setUploading(false);
+            });
+    };
+
     return (
         <main className="p-3 max-w-4xl mx-auto">
             <h1 className="text-3xl font-semibold text-center my-7">
@@ -112,15 +179,51 @@ const AddEmployee = () => {
                     <p className="font-semibold ">Image Upload</p>
                     <div className="flex gap-4">
                         <input
+                            onChange={(e) => {
+                                setFile(e.target.files[0]);
+                            }}
                             className="p-3 border border-gray-300 rounded w-full"
                             type="file"
                             id="image"
                             accept="image/*"
                         />
-                        <button className="p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80">
-                            Upload
+                        <button
+                            disabled={uploading}
+                            type="button"
+                            onClick={handleImageSubmit}
+                            className="p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80"
+                        >
+                            {uploading ? "Uploading..." : "Upload"}
                         </button>
                     </div>
+                    <p>
+                        {imageUploadError && (
+                            <span className="text-red-500">
+                                {imageUploadError}
+                            </span>
+                        )}
+                    </p>
+                    {formData.image && (
+                        <div className="flex justify-between p-3 border border-gray-300 rounded-lg items-center ">
+                            <img
+                                src={formData.image}
+                                alt="Employee"
+                                className="w-20 h-20 object-contain rounded-lg"
+                            />
+                            <button
+                                onClick={() => {
+                                    setFormData({
+                                        ...formData,
+                                        image: "",
+                                    });
+                                }} 
+                                className="p-3 bg-red-700 text-white rounded-lg uppercase hover:opacity-90"
+                                type="button"
+                            >
+                                Delete
+                            </button>
+                        </div>
+                    )}
                     <button className="p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-90 disabled:opacity-80">
                         Add Employee
                     </button>
